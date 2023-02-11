ee3dd35068e8ed75f5398f48ebd4bafa4b4bebe8
Santi Zambrino <60773712+Kyu99kyu@users.noreply.github.com>
2023-02-11 19:26:23 +0100

    starting project

---
+++ CSharp_Fundamentals/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;//System class https://learn.microsoft.com/en-us/dotnet/api/system?view=net-6.0#classes
+
+//How to comment in c#
+//Single line comment
+/*
+ * Multiple line comment
+ * Multiple line comment
+ * Multiple line comment
+ * Multiple line comment
+ */
+
+//snipe to code the selectd code Ctrl+k+c
+//Uncomment code Ctrl+Shift+ç
+//Format code Ctrl+d
+
+namespace FirstAplication//namespace: https://learn.microsoft.com/es-es/dotnet/csharp/language-reference/keywords/namespace
+{
+    internal class Program//classes: https://learn.microsoft.com/es-es/dotnet/csharp/fundamentals/types/classes
+    {
+        static void Main(string[] args)// Main method: https://learn.microsoft.com/es-es/dotnet/csharp/fundamentals/program-structure/main-command-line
+        {
+            // Console Class: https://learn.microsoft.com/en-us/dotnet/api/system.console?view=net-6.0
+            Console.WriteLine("Good luck in your journey");//WriteLine: https://learn.microsoft.com/es-es/dotnet/api/system.console.writeline?view=net-7.0
+        }
+    }
+}
+
+
---
+++ VariablesAndOperators/Program.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * 1. Crea un algoritmo que pida al usuario su nombre, su DNI y fecha de nacimiento y a
+continuación muestre esa información por pantalla de manera ordenada. Pej: Si el
+usuario se llama Juan, su DNI es 56789123D y nació el 23/02/1998 se mostrará:
+Nombre...............: Juan
+DNI..................: 56789123D
+Fecha de Nacimiento..: 23/02/1998
+ */
+using System.Text;
+
+namespace variablesAndOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Please write your name: ");
+                string? name = Console.ReadLine();
+                Console.Write("Please write your id: ");
+                string? id = Console.ReadLine();
+                Console.Write("Please enter your birth date(dd/mm/yyyy): ");
+                string? userDate = Console.ReadLine();
+
+                while ((userDate.Length == 0 || name.Length == 0 || id.Length == 0) || (name == null || id == null || userDate == null))
+                {
+                    if (name.Length == 0 || name == null)
+                    {
+                        Console.WriteLine("ERROR");
+                        Console.Write("Please enter your name again");
+                        name = Console.ReadLine();
+                    }
+                    else if (id.Length == 0 || id == null)
+                    {
+                        Console.WriteLine("ERROR");
+                        Console.Write("Please enter id again");
+                        id = Console.ReadLine();
+                    }
+                    else if (userDate.Length == 0 || userDate == null)
+                    {
+                        Console.WriteLine("ERROR");
+                        Console.Write("Please enter your birth date(dd / mm / yyyy)");
+                        userDate = Console.ReadLine();
+                    }
+
+                }
+                DateOnly birhtDate = DateOnly.Parse(userDate);
+                GetMessage(name, id, birhtDate);
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine("ERROR: The birth date format is incorrect. Please enter a date in the format dd/mm/yyyy");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(fe.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+                throw;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType());
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ForegroundColor = ConsoleColor.White;
+                throw;
+            }
+
+        }
+        public static void GetMessage(string name, string id, DateOnly birthDate)
+        {
+            StringBuilder sp = new StringBuilder();
+            sp.Append($"Name..........: {name}\n");
+            sp.Append($"Id..........: {id}\n");
+            sp.Append($"Birth Date..........: {birthDate}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(sp.ToString());
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
